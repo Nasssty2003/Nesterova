c7bff8f0fe139f90a2a49a4d67f34f0e497cd1e1
nasssty2003 <nasssty2003@gmail.com>
2022-12-18 00:57:14 +0500

    Создание csv файлов по годам

--- 3.2/3.2.1.py
+++ 3.2/3.2.1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-name_file = "vacancies_medium.csv"
-dataframe = pd.read_csv(name_file, on_bad_lines='skip')
+file = "vacancies_medium.csv"
+dataframe = pd.read_csv(file, on_bad_lines='skip')
 dataframe["years"] = dataframe["published_at"].apply(lambda date: int(date[:4]))
 all_years = list(dataframe["years"].unique())
 for year in all_years:
