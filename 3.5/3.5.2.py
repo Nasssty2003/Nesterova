cc42e742f9c6aaa060658c16ddf7f4f05ec25f3c
nasssty2003 <nasssty2003@gmail.com>
2022-12-30 23:35:19 +0500

    Реализованно сохранение вакансий в базе данных

--- 3.5/3.5.2.py
+++ 3.5/3.5.2.py
@@ -2,33 +2,38 @@ import math
 import pandas as pd
 import sqlite3
 
+currencies = {"BYR": 1, "EUR": 2, "KZT": 3, "UAH": 4, "USD": 5}
+dataframe = pd.read_csv("vacancies_dif_currencies.csv")
+connection = sqlite3.connect("3.5.1.sqlite")
+first_cursor = connection.cursor()
+dataframe["published_at"] = dataframe["published_at"].apply(lambda date: date[:7])
 
-def get_salary(row):
-    salary_from, salary_to, salary_currency, published_at = row.loc['salary_from'], row.loc['salary_to'],\
-                                                            row.loc['salary_currency'], row.loc['published_at']
-    currency1 = 0
-    if salary_currency in ['BYN', 'BYR', 'EUR', 'KZT', 'UAH', 'USD']:
+def process_salaries(row):
+    value_of_currency = 0
+    salary_currency = row.loc['salary_currency']
+    published_at = row.loc['published_at']
+    salary_from = row.loc['salary_from']
+    salary_to = row.loc['salary_to']
+    if salary_currency == 'RUR':
+        value_of_currency = 1
+    elif salary_currency in ['BYN', 'BYR', 'EUR', 'KZT', 'UAH', 'USD']:
         salary_currency.replace('BYN', 'BYR')
-        currency1 = cur.execute("SELECT * FROM currencies WHERE date == :published_at",
-                                     {"published_at": published_at}).fetchone()[currencies_col_id[salary_currency]]
-    elif salary_currency == 'RUR':
-        currency1 = 1
-    if math.isnan(salary_to) or math.isnan(salary_from):
-        salary = salary_to if math.isnan(salary_from) else salary_from
-    else:
-        salary = math.floor((salary_from + salary_to) / 2)
-    if math.isnan(salary):
-        return salary
-    return math.floor(salary * currency1)
+        value_of_currency = \
+            first_cursor.execute("SELECT * FROM currencies WHERE date == :published_at",
+                                 {"published_at": published_at}).fetchone()[
+                currencies[salary_currency]]
 
-currencies_col_id = {"BYR": 1, "EUR": 2, "KZT": 3, "UAH": 4, "USD": 5}
-df = pd.read_csv("vacancies_dif_currencies.csv")
-con = sqlite3.connect("3.5.1.sqlite")
-cur = con.cursor()
-df["published_at"] = df["published_at"].apply(lambda date: date[:7])
-df['salary'] = df.apply(lambda x: get_salary(x), axis=1)
-df.drop(columns=['salary_from', 'salary_to', 'salary_currency'], inplace=True)
+    if math.isnan(salary_to) or math.isnan(salary_from):
+        res_salary = salary_to if math.isnan(salary_from) else salary_from
+    else:
+        res_salary = math.floor((salary_from + salary_to) / 2)
+    if not (math.isnan(res_salary)):
+        return math.floor(res_salary * value_of_currency)
+    return res_salary
+
+dataframe['salary'] = dataframe.apply(lambda x: process_salaries(x), axis=1)
+dataframe.drop(columns=['salary_from', 'salary_to', 'salary_currency'], inplace=True)
 connect = sqlite3.connect("vacancies_dif_currencies.sqlite")
-cursor = con.cursor()
-df.to_sql(name="vacancies_dif_currencies", con=connect, if_exists='replace', index=False)
+cursor = connection.cursor()
+dataframe.to_sql(name="vacancies_dif_currencies", con=connect, if_exists='replace', index=False)
 connect.commit()
